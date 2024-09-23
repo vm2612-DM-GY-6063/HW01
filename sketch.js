6846c43af958aa3daf4b8a783f6e1edc338e134a
Vadivelan Murugesan <vm2612@nyu.edu>
2024-09-22 22:38:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(220, 20, 120);
-  print(width, height);
+
 }
 
 function draw() {
+  background(220, 20, 120);
+
+  rect(10, 10, 80);
+  ellipse(200, 200, 100);
+  
+  rect(10, 100, 80, 40);
+  ellipse(300, 200, 100, 150);
 }
